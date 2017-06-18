a448472ddbacec30cfd4b10facffccae36f52e2b
PakkuDon <pakku.pd@gmail.com>
2017-06-18 13:19:58 +1000

    Add foundations and funds images

--- src/assets/funds/images.js
+++
@@ -1,8 +0,0 @@
-const fundImages = {
-  fundone: require('./test.png'),
-  fundtwo: require('./test1.png'),
-  fundthree: require('./test2.png'),
-  fundfour: require('./test2.png')
-}
-
-export default fundImages;
---
+++ src/assets/funds/index.js
@@ -0,0 +1,13 @@
+import australianUnity from './australian unity672x672.png';
+import bupa from './bupa 300x300.png';
+import gmhba from './gmhba400x400.jpeg';
+import medibank from './medibank800x252.png';
+import nib from './nib420x281.png';
+
+export default {
+  australianUnity,
+  bupa,
+  gmhba,
+  medibank,
+  nib
+}
