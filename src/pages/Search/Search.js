9e0392269f8e89ec1db5939ba11d44d8de4dd511
Prawana Perera <prawana.perera@gmail.com>
2017-06-18 10:26:12 +1000

    imported styles and js

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import {Button, Icon} from 'react-materialize'
+
 
 class Search extends Component {
   render() {
     return (
       <div>
         <h1>Exisitig Policy?</h1>
+        <div>
+          <Button waves='light'>Yes<Icon left>save</Icon></Button>
+        </div>
       </div>
     );
   }
