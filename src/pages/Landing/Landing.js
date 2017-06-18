6c42db3e159618f8d35bb0482c127a1a31264f22
PakkuDon <pakku.pd@gmail.com>
2017-06-18 12:43:12 +1000

    Add copy to Landing page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
 import foundationImages from '../../assets/foundations/images';
 import fundImages from '../../assets/funds/images';
@@ -12,7 +13,7 @@ class Landing extends Component {
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 5000,
       pauseOnHover: true
     };
 
@@ -27,26 +28,38 @@ class Landing extends Component {
             }, this)
           }
         </div>
-        <hr/>
-        Landing
 
-        <div className="float: left; width: 480px;">
+        <div className='landing-wrapper'>
+          <div className='left'>
+            <ul>
+              <li><i className='fa fa-check' /> Exclusively deal in health products</li>
+              <li><i className='fa fa-check' /> Find a policy right for you</li>
+              <li><i className='fa fa-check' /> It costs no more no matter where you buy</li>
+              <li><i className='fa fa-check' /> Buy through C2C and support Stroke Foundation</li>
+            </ul>
+          </div>
 
+          <div className='right'>
+            <Slider {...settings}>
+              <div><h3>1</h3></div>
+              <div><h3>2</h3></div>
+              <div><h3>3</h3></div>
+              <div><h3>4</h3></div>
+              <div><h3>5</h3></div>
+              <div><h3>6</h3></div>
+            </Slider>
+
+            <Link to='/search'>
+              <button>
+                Compare now
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className="float: right; width: 480px;">
-          <Slider {...settings}>
-            <div><h3>1</h3></div>
-            <div><h3>2</h3></div>
-            <div><h3>3</h3></div>
-            <div><h3>4</h3></div>
-            <div><h3>5</h3></div>
-            <div><h3>6</h3></div>
-          </Slider>
 
-        </div>
         <div className="Landing-footer">
           {
-            Object.keys(fundImages).map(function (index) { 
+            Object.keys(fundImages).map(function (index) {
               return (
                 <img key={index} src={fundImages[index]} className="fund-logo" alt="fund-logo" height="100px" />
               );
