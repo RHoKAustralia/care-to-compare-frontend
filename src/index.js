705e47e5709557f8f742246f0a9f59fdfec651ff
PakkuDon <pakku.pd@gmail.com>
2017-07-20 21:38:58 +1000

    Import third-party CSS from node_modules

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+import 'materialize-css/dist/css/materialize.min.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
