ab51fdfe3a79e6835fa9a66ececb152dfce30b98
PakkuDon <pakku.pd@gmail.com>
2017-07-22 23:53:19 +1000

    Remove Materialize.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-import 'materialize-css/dist/css/materialize.min.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import 'font-awesome/css/font-awesome.min.css'
