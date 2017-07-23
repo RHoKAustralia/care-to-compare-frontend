e3f3f8529e5eb36156ae23f778158e437e38ea75
PakkuDon <pakku.pd@gmail.com>
2017-07-24 00:07:36 +1000

    Add initial story

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,10 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+import { Button } from '../components'
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add('with text', () => <Button onClick={action('clicked')}>Hello World</Button>);
