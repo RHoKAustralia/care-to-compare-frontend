da744a7faa1c0bc3377655732b5ddbab6fc7a6c4
PakkuDon <pakku.pd@gmail.com>
2017-10-01 23:59:11 +1100

    Add content to thanks page

--- src/pages/Thanks/Thanks.js
+++ src/pages/Thanks/Thanks.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 
+import { Button } from '../../components'
+
 class Thanks extends Component {
   render() {
     return (
       <div>
-        Thanks
+        <div>
+          <p>Thanks Jack</p>
+          <p>Buying health insurance through Care to Compare? is good for you and good for charity</p>
+        </div>
+        <div>
+          <h2>Your health insurance</h2>
+          <ul>
+            <li>Smart combination</li>
+            <li>$279.05 per month</li>
+            <li>Hospital and extras cover</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Your charity</h2>
+          You'll be saving $43.10 per month with your new health insurance policy
+          and supporting the Stroke Foundation with an extra $10 per month
+        </div>
+        <div>
+          <h2>Jane Survivor Story</h2>
+          <p>
+            My name is Jane and I am 43 years old, happy married and mum to
+            two 18 and 20 year old boys. I'm fit and healthy.
+            Normal blood pressure. Normal BMI. 12 weeks ago today, I woke up with
+            pins and needles in my right arm and leg. I'm a midwife and had worked
+            night shift the previous night, so I thought I'd slept really well
+            for once and hadn't moved much overnight (I'm usually a restless sleeper)...
+          </p>
+          <Button type='default'>Read more</Button>
+        </div>
+        <div>
+          <h2>Keep up the great work.</h2>
+          <p>Let your friends and family know about Care to Compare? Share on</p>
+          <Button>Facebook</Button>
+          <Button>Twitter</Button>
+          <Button>Instagram</Button>
+          <Button>Email</Button>
+        </div>
       </div>
     );
   }
