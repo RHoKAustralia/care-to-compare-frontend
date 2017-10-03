963132747ad1388ff77b9cb788f2510253b1517d
PakkuDon <pakku.pd@gmail.com>
2017-10-03 18:29:13 +1100

    Add inverted button style

--- src/stories/index.js
+++ src/stories/index.js
@@ -9,4 +9,5 @@ import { Button } from '../components'
 storiesOf('Button', module)
   .add('no type', () => <Button onClick={action('clicked')}>Hello World</Button>)
   .add('default', () => <Button type='default' onClick={action('clicked')}>Hello World</Button>)
+  .add('inverted', () => <Button type='inverted' onClick={action('clicked')}>Hello World</Button>)
   .add('secondary', () => <Button type='secondary' onClick={action('clicked')}>Hello World</Button>);
