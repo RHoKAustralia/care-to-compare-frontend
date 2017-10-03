3878314ae90f019f449c3bee564374f9bc710c66
PakkuDon <pakku.pd@gmail.com>
2017-10-03 18:03:58 +1100

    Replace materialize-css CDN link with npm package

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { App } from './components';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'materialize-css/dist/css/materialize.min.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import 'font-awesome/css/font-awesome.min.css'
