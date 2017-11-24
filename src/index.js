afeef9f1ac46bd0fc98572979162ba715ff7e99f
PakkuDon <pakku.pd@gmail.com>
2017-11-25 10:36:47 +1100

    Set up Provider with dummy root reducer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import { Provider } from 'react-redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 
 import 'materialize-css/dist/css/materialize.min.css'
 import 'slick-carousel/slick/slick.css'
@@ -9,5 +11,16 @@ import 'slick-carousel/slick/slick-theme.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootReducer = (state = {}, action) => { console.log(action); return state }
+
+const configureStore = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f,
+)(createStore)
+
+const store = configureStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'))
 registerServiceWorker()
