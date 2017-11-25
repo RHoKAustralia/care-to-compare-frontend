230b6c18b4d4118f35f42d507d926593ecf37cbd
PakkuDon <pakku.pd@gmail.com>
2017-11-25 15:02:52 +1100

    Remove example reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,10 @@
 export const types = {
-  EXAMPLES: {
-    EXAMPLE_ACTION: 'EXAMPLE_ACTION',
-  },
   POLICIES: {
     FETCH_POLICIES: 'FETCH_POLICIES',
     FETCH_POLICIES_REQUEST: 'FETCH_POLICIES_REQUEST',
     FETCH_POLICIES_SUCCESS: 'FETCH_POLICIES_SUCCESS',
     FETCH_POLICIES_FAILURE: 'FETCH_POLICIES_FAILURE',
   }
-}
-
-export const creators = {
-  example: (payload) => ({
-    type: types.EXAMPLES.EXAMPLE_ACTION,
-    payload: {
-      message: 'HELLO',
-    },
-  }),
 }
 
 export const fetchPolicies = searchCriteria => ({
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,18 +3,6 @@ import { reducer as formReducer } from 'redux-form'
 
 import { types } from 'actions'
 
-// export default (state = {}, action) => {
-//   switch(action.type) {
-//     case types.EXAMPLES.EXAMPLE_ACTION:
-//       return {
-//         ...state,
-//         example: action.payload.message,
-//       }
-//     default:
-//       return state
-//   }
-// }
-
 export default combineReducers({
   // ...your other reducers here
   // you have to pass formReducer under 'form' key,
