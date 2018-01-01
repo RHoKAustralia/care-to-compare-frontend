1c2c5b2c885ab399ebe516452d1d261335ac0521
PakkuDon <pakku.pd@gmail.com>
2018-01-02 01:41:00 +1100

    Extract out loading spinner

---
+++ src/components/LoadingSpinner/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const LoadingSpinner = ({ loading }) =>
+  loading ? (
+    <div className="preloader-wrapper big active">
+      <div className="spinner-layer spinner-blue-only">
+        <div className="circle-clipper left">
+          <div className="circle" />
+        </div>
+        <div className="gap-patch">
+          <div className="circle" />
+        </div>
+        <div className="circle-clipper right">
+          <div className="circle" />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <span />
+  )
+
+export default LoadingSpinner
--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -7,6 +7,7 @@ import Container from 'components/Container'
 import Card, { CardHeader, CardContent } from 'components/Card'
 import Grid, { Col } from 'components/Grid'
 import { IconMapMarker, IconMedkit, IconUser } from 'components/Icons'
+import LoadingSpinner from 'components/LoadingSpinner'
 import './styles.css'
 
 class SearchResults extends Component {
@@ -21,63 +22,48 @@ class SearchResults extends Component {
         </div>
 
         <div className="search-results-wrapper">
-          {this.props.policySearch.loading ? (
-            <div className="preloader-wrapper big active">
-              <div className="spinner-layer spinner-blue-only">
-                <div className="circle-clipper left">
-                  <div className="circle" />
-                </div>
-                <div className="gap-patch">
-                  <div className="circle" />
-                </div>
-                <div className="circle-clipper right">
-                  <div className="circle" />
-                </div>
-              </div>
+          <LoadingSpinner loading={this.props.policySearch.loading} />
+          <Container>
+            <div className="summary">
+              <Card>
+                <CardHeader>
+                  <h3 className="top">Summary</h3>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    <li>
+                      <IconUser />
+                      <span>Individual</span>
+                    </li>
+                    <li>
+                      <IconMapMarker /> Location
+                    </li>
+                    <li>
+                      <IconMedkit /> Hospital mid level & extras top level
+                    </li>
+                    <li> ${} excess</li>
+                  </ul>
+                </CardContent>
+              </Card>
             </div>
-          ) : (
-            <Container>
-              <div className="summary">
-                <Card>
-                  <CardHeader>
-                    <h3 className="top">Summary</h3>
-                  </CardHeader>
-                  <CardContent>
-                    <ul>
-                      <li>
-                        <IconUser />
-                        <span>Individual</span>
-                      </li>
-                      <li>
-                        <IconMapMarker /> Location
-                      </li>
-                      <li>
-                        <IconMedkit /> Hospital mid level & extras top level
-                      </li>
-                      <li> ${} excess</li>
-                    </ul>
-                  </CardContent>
-                </Card>
-              </div>
 
-              <Grid>
-                {this.props.policySearch.searchResults
-                  .slice(0, 3)
-                  .map((policy) => (
-                    <Col s={4}>
-                      <PolicyStub key={policy.sisCode} policy={policy} />
-                    </Col>
-                  ))}
-                {this.props.policySearch.searchResults.length === 0 && (
-                  <Col s={12}>
-                    <Card>
-                      <CardContent>No results found.</CardContent>
-                    </Card>
+            <Grid>
+              {this.props.policySearch.searchResults
+                .slice(0, 3)
+                .map((policy) => (
+                  <Col s={4}>
+                    <PolicyStub key={policy.sisCode} policy={policy} />
                   </Col>
-                )}
-              </Grid>
-            </Container>
-          )}
+                ))}
+              {this.props.policySearch.searchResults.length === 0 && (
+                <Col s={12}>
+                  <Card>
+                    <CardContent>No results found.</CardContent>
+                  </Card>
+                </Col>
+              )}
+            </Grid>
+          </Container>
         </div>
 
         <CharityBanner />
