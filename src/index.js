877054998d2160f8342b9455c1e632cc7a6a4b72
Prawana Perera <prawana.perera@gmail.com>
2018-01-23 18:57:55 +1100

    integrated graphql appolo client, quering backend graphql API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
@@ -14,6 +18,16 @@ import './index.css'
 import rootReducer from 'reducers'
 import rootSaga from 'sagas'
 
+const API_ENDPOINT = process.env.API_ENDPOINT || 'http://localhost:4000/graphql'
+const httpLink = new HttpLink({
+  uri: API_ENDPOINT,
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
 const sagaMiddleware = createSagaMiddleware()
 
 const configureStore = compose(
@@ -25,9 +39,11 @@ const store = configureStore(rootReducer)
 sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ApolloProvider store={store} client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root'),
 )
 registerServiceWorker()
--- src/pages/Search/Step3.js
+++ src/pages/Search/Step3.js
@@ -1,31 +1,166 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getFormValues } from 'redux-form'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import lodash from 'lodash'
 
 import { formName } from './constants'
 
-let Step3 = (props) => {
-  const { handleSubmit, onPrevious, searchCriteria } = props
-  console.log(searchCriteria)
+class Step3 extends Component {
+  state = { page: 1 }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h2>Search Results (Placeholder for search results)</h2>
+  handleNext = () => {
+    const nextPage = this.state.page + 1
 
+    this.props.loadPage(nextPage).then(() => {
+      this.setState(({ page }) => ({ page: nextPage }))
+    })
+  }
+
+  handlePrevious = () => {
+    const prevPage = this.state.page - 1
+
+    this.props.loadPage(prevPage).then(() => {
+      this.setState(({ page }) => ({ page: prevPage }))
+    })
+  }
+
+  render() {
+    const {
+      handleSubmit,
+      onPrevious,
+      searchCriteria,
+      loading,
+      error,
+      policies,
+      meta,
+    } = this.props
+
+    if (loading) {
+      return <div>Loading...</div>
+    }
+
+    if (error) {
+      return <div>{`Errors: ${error}`}</div>
+    }
+
+    return (
       <div>
-        <h4>Search Summary</h4>
-        <p>TBA from searchCriteria</p>
-        <p>TBA from searchCriteria</p>
-        <p>TBA from searchCriteria</p>
+        <h3 className="f3 lh-copy">Search Results</h3>
+        <blockquote>
+          {`Page ${meta.page} of ${meta.totalPages}`} (Total results:{' '}
+          {meta.totalRecords})
+        </blockquote>
+        <div>
+          {policies.map((policy) => (
+            <div key={policy.id}>
+              <div>
+                {policy.policyName} - ${policy.monthlyPremium}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="mt3">
+          {meta.page > 1 ? (
+            <button
+              className="f6 link dim br1 ba bw2 ph3 pv2 mb2 dib mid-gray"
+              onClick={this.handlePrevious}
+            >
+              &lt;&lt;&lt; Previous
+            </button>
+          ) : (
+            <span />
+          )}
+          {meta.page < meta.totalPages ? (
+            <button
+              className="f6 link dim br1 ba bw2 ph3 pv2 mb2 dib mid-gray"
+              onClick={this.handleNext}
+            >
+              Next &gt;&gt;&gt;
+            </button>
+          ) : (
+            <span />
+          )}
+        </div>
       </div>
-
-      <button type="button" onClick={onPrevious}>
-        Update Search
-      </button>
-      <button type="submit">Search</button>
-    </form>
-  )
+    )
+  }
 }
+
+const POLICIES_QUERY = gql`
+  query(
+    $policyType: PolicyType!
+    $categoryOfCover: CategoryOfCover!
+    $stateOfResidence: AustralianStates!
+    $page: Int!
+    $hospitalInclusions: [HospitalInclusions]
+    $extrasInclusions: [ExtrasInclusions]
+  ) {
+    search(
+      policyType: $policyType
+      categoryOfCover: $categoryOfCover
+      state: $stateOfResidence
+      hospitalInclusions: $hospitalInclusions
+      extrasInclusions: $extrasInclusions
+      page: $page
+      pageSize: 3
+    ) {
+      policies {
+        id
+        fundName
+        policyName
+        monthlyPremium
+      }
+      meta {
+        page
+        pageSize
+        totalPages
+        totalRecords
+      }
+    }
+  }
+`
+
+const mapPropsToOptions = (props) => {
+  const { searchCriteria } = props
+  return {
+    notifyOnNetworkStatusChange: true,
+    variables: {
+      policyType: searchCriteria.policyType,
+      categoryOfCover: searchCriteria.categoryOfCover,
+      stateOfResidence: searchCriteria.stateOfResidence,
+      hospitalInclusions: searchCriteria.hospitalInclusions
+        ? searchCriteria.hospitalInclusions
+        : [],
+      extrasInclusions: searchCriteria.extrasInclusions
+        ? searchCriteria.extrasInclusions
+        : [],
+      page: 1,
+    },
+  }
+}
+
+const mapResultsToProps = ({ data }) => {
+  return {
+    // https://github.com/apollographql/react-apollo/issues/1385
+    loading: data.loading,
+    error: data.error,
+    policies: data.search ? data.search.policies : [],
+    meta: data.search ? data.search.meta : {},
+    loadPage: (page) =>
+      data.fetchMore({
+        variables: { page },
+        updateQuery: (previousResult, { fetchMoreResult }) =>
+          lodash.merge({}, previousResult, fetchMoreResult),
+      }),
+  }
+}
+
+Step3 = graphql(POLICIES_QUERY, {
+  props: mapResultsToProps,
+  options: mapPropsToOptions,
+})(Step3)
 
 Step3 = connect((state) => ({
   searchCriteria: getFormValues(formName)(state),
