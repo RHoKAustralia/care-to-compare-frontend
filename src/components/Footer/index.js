0040cc18b2e8644711a7c2ddfd63fa434cd7bccb
PakkuDon <pakku.pd@gmail.com>
2018-06-02 11:00:41 +1000

    Add footer with links to terms, policies, etc

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,8 +7,26 @@ import styles from './styles.css'
 class Footer extends Component {
   render() {
     return (
-      <footer className={styles.footer}>
-        <Container />
+      <footer className={styles.root}>
+        <Container>
+          <h2 className={styles.subheading}>Customer info</h2>
+          <ul>
+            <li>How Care to Compare works</li>
+            <li>
+              <a href="https://caretocompare.com.au/privacy-policy">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="https://caretocompare.com.au/website-terms">
+                Website Terms
+              </a>
+            </li>
+            <li>
+              <a href="https://caretocompare.com.au/faq">FAQ</a>
+            </li>
+          </ul>
+        </Container>
       </footer>
     )
   }
