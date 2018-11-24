ecefb8184ce546e3088b62f2e2631754343f0938
kitperez <kit@crystaldelta.com>
2018-11-24 16:00:25 +1100

    drop landing page and point straight to start page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -17,8 +17,7 @@ class App extends Component {
         <div>
           <Header />
           <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route path="/search" component={Search} />
+            <Route exact path="/" component={Search} />
             <Route path="/results" component={SearchResults} />
             <Route path="/checkout" component={Checkout} />
             <Route path="/thanks" component={Thanks} />
